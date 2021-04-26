16e3e8cc3c72d5db9e1c5b6dc79d5b616941f4e4
UC-VR <53860099+UC-VR@users.noreply.github.com>
2021-04-26 07:03:18 +0300

    added config to ignore

--- run_config.py
+++ run_config.py
@@ -5,15 +5,35 @@ one per account/sub
 
 Binance_APIs = [
     {
-         'account_name': 'Binance Futures - Main'
-        ,'Binance_API_KEY': ''
-        ,'Binance_API_SECRET': ''
+         'account_name': 'X1'
+        ,'Binance_API_KEY': '1vy0Nhraze0wUU5Q9dyl3U4shwrKoDDOE581BcVWQu13Qt9igAAZcVER5N3qPmW2 '
+        ,'Binance_API_SECRET': 'qZEslW7UfVhnrjyOqdXHGAE3i6jlD4FEB9lQM1OhZiUyIQy83auH4JRbb17yOp2d'
     }
     ,{
-         'account_name': 'Binance Futures - Sub 01'
-        ,'Binance_API_KEY': ' '
-        ,'Binance_API_SECRET': ''
+         'account_name': 'X2'
+        ,'Binance_API_KEY': '8w71Sd5HLm8jcL58etYsESdaJOar7AUO2i7PM0Lhu0hOJDQyf58Rq9kdQMjHSOGy'
+        ,'Binance_API_SECRET': 'mQRcccnM8v2rEbTZp6Rdt0LXRl8XXhw9248H3lv2YNtEa8tl1TklekldwNeMekk9'
     }
+    ,{
+         'account_name': 'X3'
+        ,'Binance_API_KEY': '7S0EKiMv53SK1yowUXm5EbdVh4fahBPOMS4iYwG8i1MHjrRQAYk74WUrsOe6Bygi'
+        ,'Binance_API_SECRET': 'hVrcXS7JQUyfCSvSOcBDPfGmGpAS0tDLCCQMmQkIfczLXfTbz2HNmN71Um3R1ztm'
+    }
+    ,{
+         'account_name': 'X4'
+        ,'Binance_API_KEY': 'TvRFswZyTak40gXgRMBqWJS7GblBiwUydM3A3HQhY3D6YXm4V3jjhKoiFSoGM8bk'
+        ,'Binance_API_SECRET': 'Ral0RpiOj16cpbS951LMW7Uy44et3bfwip1Nfx8K4eTBV2WKiUMfGBAfPWDZ8inq'
+    }
+    ,{
+         'account_name': 'X5'
+        ,'Binance_API_KEY': 'u2VCqP44TZVOTzObbhRE58TSTkNHvdDNAycbjo0kYP26LTtljRccp5phMJAoTdhG'
+        ,'Binance_API_SECRET': 'fOqR8hgNlwmyttdTIBi1ESTkYFLZYLoYzlMJoFXEHH47UmaaWvEJWihUO7a9KU1e'
+    }
+    ,{
+         'account_name': 'X6'
+        ,'Binance_API_KEY': 'P47zflNy83BJ1rcQ2UDafpybBITNwrjpQapmOGVkk8AWaqWEvKz2T55OnYWT7PPy'
+        ,'Binance_API_SECRET': 'BLocoXQheGY7mVPvYWf9ckxJIRe17gtIUIjcJpEo9BmXpU4DsOD8xrHSxc8gmfOF'
+    }    
 ]
 
 
@@ -21,12 +41,12 @@ Binance_APIs = [
 '''
 Create an API key/secret from https://3commas.io/api_access_tokens/new with BotsRead, BotsWrite, and AccountsRead
 '''
-TCommas_API_KEY = ''
-TCommas_API_SECRET = ''
+TCommas_API_KEY = '1c86c29c4c32413fa83b4744ef4d31517a076b5d66ca42a19c46d516d2484cc4'
+TCommas_API_SECRET = '5f740777bb99b92011e4736a5051741d45e76252730df9f07feb2f14cd1230a74f8cd4ebf1bf8fb536883fd1e2ba4fea61dd0542cc2878990f052f73a7b973f56ad9932ef99eba9b655a85d9ac5b6749b13f26d968f6701d237bec8cbc8a6408d1c41ef0'
 
 
 
 '''
 Optional
 '''
-# ifttt_url = ''
+# ifttt_url = 'https://maker.ifttt.com/trigger/test1/with/key/jcFC9HBpUF8kB98ZZe-o9r70HTxOMaFkq0idIeXOF5-'
